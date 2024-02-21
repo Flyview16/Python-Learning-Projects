ba286cf6b4a64f6b2c12108b4361bcd3b20adb71
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-02-21 11:51:20 +0000

    Complete hangman game in python

--- Projects/7-Hangman/main.py
+++ Projects/7-Hangman/main.py
@@ -1,72 +1,19 @@
 import random
+import hangman_words
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_art import logo, stages
 
-word_list = ["aardvark", "baboon", "camel"]
+#Display hangman logo
+print(logo)
+
 end_of_game = False
 lives = 6
 
 #Randomly choose a word from the word_list 
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 display = []
 for num in chosen_word:
     display.append("_")
-print(display)
 
 
 while not end_of_game:
@@ -74,6 +21,9 @@ while not end_of_game:
     #Ask the user to guess a letter
     guess = input("Guess a letter: ").lower()
 
+    #Check if guessed letter has already been guessed
+    if guess in display:
+        print(f"You've already guessed {guess}")
     # Check if the letter the user guessed is one of the letters in the chosen_word and replace in display.
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
@@ -82,10 +32,13 @@ while not end_of_game:
 
     #Check if guess is not in chosen_word
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
             end_of_game = True
             print("You lose")
+    
+    print(f"{''.join(display)}")
 
     # Check if user got all letters
     if "_" not in display:
