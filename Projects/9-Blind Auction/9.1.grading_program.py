48341736a2b25da0548aa12c2470d0b6ab1249e3
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-02-23 13:55:57 +0000

    Working with dictionaries

---
+++ Projects/9-Blind Auction/9.1.grading_program.py
@@ -0,0 +1,28 @@
+
+#Dictionary to store student names and grades
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+
+#Dictionary to store student names and grade remarks
+student_grades = {}
+
+#Adding remarks to student_grades
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectation"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+    
+#Output
+print(student_grades)
