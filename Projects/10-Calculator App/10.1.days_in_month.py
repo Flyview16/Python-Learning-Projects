72368c36a49776cc2caf4fa0ef5a8c642bfb45fb
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-04 17:41:45 +0000

    Re-commenting code

--- Projects/10-Calculator App/10.1.days_in_month.py
+++ Projects/10-Calculator App/10.1.days_in_month.py
@@ -1,3 +1,4 @@
+#Check if year is a leap year
 def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
@@ -10,11 +11,18 @@ def is_leap(year):
   else:
     return False
 
-def days_in_month():
+#Return number of days in month of a non-leap year
+def days_in_month(year, month):
+  if month > 12 or month < 1:
+    return "Invalid input"
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+
+  #if year is leap year, return number of days in February
+  if is_leap(year) and month == 2:
+    return 29
+  return month_days[month - 1]
   
-  
-#🚨 Do NOT change any of the code below 
+#Output
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
