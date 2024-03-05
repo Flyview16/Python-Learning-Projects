f84bc6d16328caaadcd027443e58191d4155aea2
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-05 11:19:45 +0000

    Building calculator program

---
+++ Projects/10-Calculator App/10.2.calculator_app.py
@@ -0,0 +1,54 @@
+#Calculator
+from art import logo
+
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+#Multiply
+def multipy(n1, n2):
+    return n1 * n2
+
+#Divide
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multipy,
+    "/" : divide,
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What is the first number? "))
+
+    for symbol in operations:
+        print(f"{symbol}")
+
+    should_continue = True
+
+    while should_continue:
+        operation = input("Pick an operation: ")
+
+        num2 = float(input("What is the next number? "))
+
+        function = operations[operation]
+        result = function(num1, num2)
+
+        print(f"{num1} {operation} {num2} = {result}")
+
+        if input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = result
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
