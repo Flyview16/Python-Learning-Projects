7dc1888dfb2d0fca76bef94f0091cce3ff14e6d7
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-06 18:42:11 +0000

    Modularising code using functions

--- Projects/14-Higher Lower Game/main.py
+++ Projects/14-Higher Lower Game/main.py
@@ -1,36 +1,64 @@
-from art import logo
-from art import vs
+from art import logo, vs
 import random
 from game_data import data
+
+def format_data(option):
+    """Take option data and return printable format"""
+    option_name = option['name']
+    option_descr = option['description']
+    option_country = option['country']
+    return f"{option_name}, a {option_descr}, from {option_country}"
+
+def check_answer(choice, a_followers, b_followers):
+    """Takes choice and compares followers to see it player got it right."""
+    if a_followers > b_followers:
+        return choice == "a"
+    else:
+        return choice == "b"
+    
 
 score = 0
 game_over = False
 option1 = random.choice(data)
 option2 = random.choice(data)
+if option1 == option2:
+    option2 = random.choice(data)
 
-while not game_over:
-    #print(logo)
 
-    print(f"Compare A: {option1['name']}, a {option1['description']}, from {option1['country']} ")
-    #print(vs)
-    print(f"Against B: {option2['name']}, a {option2['description']}, from {option2['country']}")
+#print(logo)
 
-    choice = input("Who has more followers? Type 'A' or 'B': ")
+print(f"Compare A: {format_data(option1)}")
+#print(vs)
+print(f"Against B: {format_data(option2)}")
 
-    if choice == 'A':
-        if option1['follower_count'] >= option2['follower_count']:
-            score += 1
-            option1 = option2
-            option2 = random.choice(data)
-        else:
-            game_over = True
-    elif choice == 'B':
-        if option2['follower_count'] >= option1['follower_count']:
-            score += 1
-            option1 = option2
-            option2 = random.choice(data)
-        else:
-            game_over = True
+choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+a_follower_count = option1['follower_count']
+b_follower_count = option2['follower_count']
+
+is_correct = check_answer(choice, a_follower_count, b_follower_count)
+
+#Feedback and keeping track of score
+if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}")
+else:
+    print(f"Sorry you're wrong. Final score: {score}")
+
+if choice == 'A':
+    if option1['follower_count'] >= option2['follower_count']:
+        score += 1
+        option1 = option2
+        option2 = random.choice(data)
     else:
-        print("Invalid input. Start again.")
         game_over = True
+elif choice == 'B':
+    if option2['follower_count'] >= option1['follower_count']:
+        score += 1
+        option1 = option2
+        option2 = random.choice(data)
+    else:
+        game_over = True
+else:
+    print("Invalid input. Start again.")
+    game_over = True
