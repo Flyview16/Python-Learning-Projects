eedb83633b8ed0e08d754e1ae63e5d0c93cdafed
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-14 22:34:23 +0000

    Using prettytable package

--- Projects/16-Object Oriented Programming/16.1.objects_and_classes.py
+++ Projects/16-Object Oriented Programming/16.1.objects_and_classes.py
@@ -1,1 +1,7 @@
 # Using python packages. PrettyTable for ASCII tables
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
