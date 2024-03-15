4abe517d169d35af7066aa5180994c0e631020f1
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-15 13:54:05 +0000

    Drawing short dashes with turtle

--- Projects/18-Turtle and GUI/main.py
+++ Projects/18-Turtle and GUI/main.py
@@ -3,10 +3,18 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape("turtle")
 
-# Draw a square with Tim the turtle
-for i in range (4):
-    tim.forward(100)
-    tim.left(90)
+# # Draw a square with Tim the turtle
+# for i in range (4):
+#     tim.forward(100)
+#     tim.left(90)
+
+# Draw short dashes
+for i in range(10):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
 
 
 
