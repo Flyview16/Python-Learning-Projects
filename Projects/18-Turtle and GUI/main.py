8d86d6a1d60fce9b5d6225e9de954a36ecff8e96
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-15 14:17:57 +0000

    Drawing different shapes with different colors

--- Projects/18-Turtle and GUI/main.py
+++ Projects/18-Turtle and GUI/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tim.shape("turtle")
@@ -8,13 +9,21 @@ tim.shape("turtle")
 #     tim.forward(100)
 #     tim.left(90)
 
-# Draw short dashes
-for i in range(10):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+# # Draw short dashes
+# for i in range(10):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
+# Draw shapes from triangle to decagon
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+for sides in range(3, 11):
+    tim.color(random.choice(colors))
+    for i in range(sides):
+        tim.forward(100)
+        tim.right(360 / sides)
 
 
 
