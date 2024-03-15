092d6f84facef0a160cd26d499603483bd11634c
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-15 13:33:40 +0000

    Getting started with Turtle and GUI

---
+++ Projects/18-Turtle and GUI/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+
+# Draw a square with Tim the turtle
+for i in range (4):
+    tim.forward(100)
+    tim.left(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
