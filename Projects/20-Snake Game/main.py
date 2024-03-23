ec98296bf0c80af0f67b082667ee1d29863a3212
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-23 17:02:12 +0000

    Adding collision detection with wall

--- Projects/20-Snake Game/main.py
+++ Projects/20-Snake Game/main.py
@@ -30,9 +30,15 @@ while game_on:
     time.sleep(0.1)
     snake.move()
 
-    # Detect Collision
+    # Detect Collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    # Detect Collision with wall
+    if snake.head.xcor() > 288 or snake.head.xcor() < -288 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+        game_on = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
