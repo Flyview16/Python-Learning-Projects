d2c92ba05a96fee77b19b39bd4bbe387e4463c30
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-03-23 16:10:26 +0000

    Building the food for the snake game

--- Projects/20-Snake Game/main.py
+++ Projects/20-Snake Game/main.py
@@ -1,17 +1,18 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
-
 
 # Setting up screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-screen.tracer(0)    # turn off animation
+screen.tracer(0)  # turn off animation
 
-# Creating snake body
+# Creating snake body and food
 snake = Snake()
+food = Food()
 
 # Set listener to monitor keystrokes
 screen.listen()
@@ -20,28 +21,12 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 # Move snake
 game_on = True
 while game_on:
-    screen.update()     # update the screen
+    screen.update()  # update the screen
     time.sleep(0.1)
     snake.move()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
