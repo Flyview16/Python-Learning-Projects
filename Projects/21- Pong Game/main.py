e89de8f3a0a32411179e91612560f6143b394470
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-04-30 05:43:29 +0000

    Creating ball object

--- Projects/21- Pong Game/main.py
+++ Projects/21- Pong Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 
 # Screen Setup
@@ -9,6 +11,7 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)  # turn off animation
 
+# Create paddle objects
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
@@ -19,9 +22,13 @@ screen.onkey(r_paddle.go_down, key="Down")
 screen.onkey(l_paddle.go_up, key="w")
 screen.onkey(l_paddle.go_down, key="s")
 
+# Create ball object
+ball = Ball()
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.2)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
