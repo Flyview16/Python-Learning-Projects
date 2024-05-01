194625a074c5fea366862fc1ccd15b37669d830b
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-01 22:28:06 +0000

    Creating player

--- Projects/22-Turtle Crossing Game/main.py
+++ Projects/22-Turtle Crossing Game/main.py
@@ -4,9 +4,16 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# Set up screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+# Set up player
+player = Player()
+
+screen.listen()
+screen.onkeypress(player.move, key="Up")
 
 game_is_on = True
 while game_is_on:
