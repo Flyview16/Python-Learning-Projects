0246df262b3b27be45a0679469f18b96283cc0e9
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-03 21:11:22 +0000

    Something I've forgotten

--- Projects/25-Working with CSV and Pandas/25.2- US States Game/main.py
+++ Projects/25-Working with CSV and Pandas/25.2- US States Game/main.py
@@ -8,22 +8,28 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# Take user input
-answer_state = screen.textinput(title="Guess the State", prompt="What is the name of a state?").capitalize()
-
 # Read csv file
 data = pandas.read_csv("50_states.csv")
 
 # Extract states
 states = data.state.to_list()
 
-# Check if answer is part of states
-if answer_state in states:
-    turtle = turtle.Turtle()
-    turtle.penup()
-    turtle.hideturtle()
-    state_data = data[data.state == answer_state]
-    turtle.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
-    turtle.write(answer_state)
+guessed_state = []
+
+
+while len(guessed_state) < 50:
+    # Take user input
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 Correct States", prompt="What is the name of a "
+                                                                                            "state?").title()
+
+    # Check if answer is part of states
+    if answer_state in states:
+        guessed_state.append(answer_state)
+        t = turtle.Turtle()
+        t.penup()
+        t.hideturtle()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+        t.write(answer_state)
 
 screen.exitonclick()
