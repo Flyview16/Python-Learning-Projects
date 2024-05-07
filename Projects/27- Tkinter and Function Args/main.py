768cbe552744dd15c502a9276080958f33dba7ae
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-07 16:44:27 +0000

    More on Tkinter components

--- Projects/27- Tkinter and Function Args/main.py
+++ Projects/27- Tkinter and Function Args/main.py
@@ -1,19 +1,27 @@
-import tkinter
+from tkinter import *
+
+
+def button_clicked():
+    my_label["text"] = user_input.get()
+
 
 # Creating a window
-window = tkinter.Tk()
+window = Tk()
 window.title("First GUI Program")
 window.minsize(width=500, height=300)
 
 
 # Creating a label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "italic"))
+my_label = Label(text="I am a label", font=("Arial", 24))
 my_label.pack()
 
+# Taking input using entry component
+user_input = Entry()
+user_input.pack()
 
-
-
-
+# Creating a button
+button = Button(text="Change label", command=button_clicked)
+button.pack()
 
 # Keep Screen on
 window.mainloop()
