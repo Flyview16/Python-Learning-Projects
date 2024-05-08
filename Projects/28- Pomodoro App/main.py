3e0896a59ccbe963c3d24d8d85d72205e4e73d9c
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-08 23:50:26 +0000

    Setting up window and background (ui)

--- Projects/28- Pomodoro App/main.py
+++ Projects/28- Pomodoro App/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+# Create canvas to set image as background
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")   # Read file to extract image
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))   # Place text on tomato image
+canvas.pack()
+
+
+window.mainloop()
