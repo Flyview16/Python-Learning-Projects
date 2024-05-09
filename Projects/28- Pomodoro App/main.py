2c179da1581a610530361c65167561a539d92d6a
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-09 00:12:41 +0000

    Complete ui setup for pomodoro app

--- Projects/28- Pomodoro App/main.py
+++ Projects/28- Pomodoro App/main.py
@@ -27,14 +27,19 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")   # Read file to extract image
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))   # Place text on tomato image
-canvas.grid(column=1, row=2)
+canvas.grid(column=1, row=1)
 
 # Labels
-timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
-check = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 28, "bold"))
+check = Label(text="🗸", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25))
 timer.grid(column=1, row=0)
 check.grid(column=1, row=3)
 
+# Buttons
+start = Button(text="Start")
+reset = Button(text="Reset")
+start.grid(column=0, row=2)
+reset.grid(column=2, row=2)
 
 
 window.mainloop()
