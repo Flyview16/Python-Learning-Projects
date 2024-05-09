acc30a931e9fde118f3c9e52e62a84964bd389f2
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-09 19:08:42 +0000

    added reset function for pomodoro app

--- Projects/28- Pomodoro App/main.py
+++ Projects/28- Pomodoro App/main.py
@@ -10,13 +10,37 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(f"{timer}")
+    canvas.itemconfig(timer_value, text="00:00")
+    timer_label.config(text="Timer")
+    check.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -27,9 +51,17 @@ def count_down(count):
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec} ")
+    canvas.itemconfig(timer_value, text=f"{count_min}:{count_sec} ")
     if count > 0:
-        canvas.after(1000, count_down, count - 1)  # Call count down funct after every 1s
+        global timer
+        timer = canvas.after(1000, count_down, count - 1)  # Call count down funct after every 1s
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for i in range(work_sessions):
+            marks += "🗸"
+        check.config(text="🗸")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -41,19 +73,19 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")  # Read file to extract image
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(103, 130, text="00:00", fill="white",
-                                font=(FONT_NAME, 35, "bold"))  # Place text on tomato image
+timer_value = canvas.create_text(103, 130, text="00:00", fill="white",
+                                 font=(FONT_NAME, 35, "bold"))  # Place text on tomato image
 canvas.grid(column=1, row=1)
 
 # Labels
-timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 28, "bold"))
-check = Label(text="🗸", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25))
-timer.grid(column=1, row=0)
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 28, "bold"))
+check = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25))
+timer_label.grid(column=1, row=0)
 check.grid(column=1, row=3)
 
 # Buttons
 start = Button(text="Start", command=start_timer)
-reset = Button(text="Reset")
+reset = Button(text="Reset", command=reset_timer)
 start.grid(column=0, row=2)
 reset.grid(column=2, row=2)
 
