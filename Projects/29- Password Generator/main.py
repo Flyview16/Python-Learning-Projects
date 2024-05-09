b77311066f0a48ead56ccd4de8c4aa6928e74c1c
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-09 22:43:57 +0000

    adding save user data functionality to password manager

--- Projects/29- Password Generator/main.py
+++ Projects/29- Password Generator/main.py
@@ -1,8 +1,23 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_user_info():
+    website_name = website_entry.get()
+    user_email = email_entry.get()
+    user_password = password_entry.get()
+
+    # write data to file, open in append mode
+    with open(file="data.txt", mode="a") as file:
+        file.write(f"{website_name} | {user_email} | {user_password}")
+
+    # Clear Text from entry box
+    website_entry.delete(0, END)
+    email_entry.delete(0, END)
+    password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -29,17 +44,17 @@ website_entry = Entry(width=52)
 email_entry = Entry(width=52)
 password_entry = Entry(width=33)
 
+website_entry.focus()
+
 website_entry.grid(column=1, row=1, columnspan=2)
 email_entry.grid(column=1, row=2, columnspan=2)
 password_entry.grid(column=1, row=3)
 
 # Buttons
 password_generator = Button(text="Generate Password")
-add_button = Button(text="Add", width=44)
+add_button = Button(text="Add", width=44, command=save_user_info)
 
 password_generator.grid(column=2, row=3)
 add_button.grid(column=1, row=4, columnspan=2)
-
-
 
 window.mainloop()
