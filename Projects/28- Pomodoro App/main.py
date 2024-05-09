e604c5b97b10d277289b586ecb23d5e3a4a2ef4d
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-09 18:17:21 +0000

    Completing timer mechanism and time format

--- Projects/28- Pomodoro App/main.py
+++ Projects/28- Pomodoro App/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -13,11 +14,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+    # Get time in format min:sec
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec} ")
     if count > 0:
         canvas.after(1000, count_down, count - 1)  # Call count down funct after every 1s
 
@@ -31,10 +41,9 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")  # Read file to extract image
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white",
                                 font=(FONT_NAME, 35, "bold"))  # Place text on tomato image
 canvas.grid(column=1, row=1)
-count_down(5)
 
 # Labels
 timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 28, "bold"))
@@ -43,7 +52,7 @@ timer.grid(column=1, row=0)
 check.grid(column=1, row=3)
 
 # Buttons
-start = Button(text="Start")
+start = Button(text="Start", command=start_timer)
 reset = Button(text="Reset")
 start.grid(column=0, row=2)
 reset.grid(column=2, row=2)
