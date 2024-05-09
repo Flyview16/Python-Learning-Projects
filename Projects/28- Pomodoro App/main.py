b002c2b53f68a89c31ab673a5e358ad64f11d2a7
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-09 00:00:06 +0000

    Adding labels timer and check labels to pomodoro

--- Projects/28- Pomodoro App/main.py
+++ Projects/28- Pomodoro App/main.py
@@ -27,7 +27,14 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")   # Read file to extract image
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))   # Place text on tomato image
-canvas.pack()
+canvas.grid(column=1, row=2)
+
+# Labels
+timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
+check = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+timer.grid(column=1, row=0)
+check.grid(column=1, row=3)
+
 
 
 window.mainloop()
