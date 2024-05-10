05ac7973c2140fbb762dc5e58ccfae61e39b1aef
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-10 00:03:25 +0000

    Adding password generator function to password manager

--- Projects/29- Password Generator/main.py
+++ Projects/29- Password Generator/main.py
@@ -1,8 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_user_info():
@@ -40,14 +58,15 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
-# Labels
-website = Label(text="Website:")
-email_username = Label(text="Email/Username:")
-password = Label(text="Password:")
 
-website.grid(column=0, row=1)
+# Labels
+website_label = Label(text="Website:")
+email_username = Label(text="Email/Username:")
+password_label = Label(text="Password:")
+
+website_label.grid(column=0, row=1)
 email_username.grid(column=0, row=2)
-password.grid(column=0, row=3)
+password_label.grid(column=0, row=3)
 
 # Text input fields
 website_entry = Entry(width=52)
@@ -61,7 +80,7 @@ email_entry.grid(column=1, row=2, columnspan=2)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-password_generator = Button(text="Generate Password")
+password_generator = Button(text="Generate Password",command=generate_password)
 add_button = Button(text="Add", width=44, command=save_user_info)
 
 password_generator.grid(column=2, row=3)
