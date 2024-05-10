54b82ae8227b7d41c8e599932b693c91b05366a7
Herbert Degadzor <herbert.s.degadzor@gmail.com>
2024-05-10 20:45:00 +0000

    Adding search functionality to password manager

--- Projects/29- Password Generator/main.py
+++ Projects/29- Password Generator/main.py
@@ -41,8 +41,8 @@ def save_user_info():
         messagebox.showwarning(title="Oops", message="Please don't leave any field(s) empty!")
     else:
         is_ok = messagebox.askokcancel(title=website_name,
-                                       message=f"These are the details of entered:\nEmail:{user_email}\n"
-                                               f"Password:{user_password}\nIs it okay to save?")
+                                       message=f"These are the details of entered:\nEmail: {user_email}\n"
+                                               f"Password: {user_password}\nIs it okay to save?")
         if is_ok:
             # Saving data to json file
             try:
@@ -61,8 +61,21 @@ def save_user_info():
             finally:
                 # Clear Text from entry box
                 website_entry.delete(0, END)
-                email_entry.delete(0, END)
                 password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website_name = website_entry.get()
+    with open(file="data.json", mode="r") as file:
+        # Read data
+        data = json.load(file)
+        if website_name in data:
+            email = data[website_name]["email"]
+            password = data[website_name]["password"]
+            messagebox.showinfo(title=website_name,
+                                message=f"Email/Username: {email}\nPassword: {password}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -78,7 +91,6 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
-
 # Labels
 website_label = Label(text="Website:")
 email_username = Label(text="Email/Username:")
@@ -89,21 +101,24 @@ email_username.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
 
 # Text input fields
-website_entry = Entry(width=52)
+website_entry = Entry(width=33)
 email_entry = Entry(width=52)
+email_entry.insert(0, "herbert@gmail.com")
 password_entry = Entry(width=33)
 
 website_entry.focus()
 
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1)
 email_entry.grid(column=1, row=2, columnspan=2)
 password_entry.grid(column=1, row=3)
 
 # Buttons
 password_generator = Button(text="Generate Password", command=generate_password)
 add_button = Button(text="Add", width=44, command=save_user_info)
+search_button = Button(text="Search", width=14, command=find_password)
 
 password_generator.grid(column=2, row=3)
 add_button.grid(column=1, row=4, columnspan=2)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
